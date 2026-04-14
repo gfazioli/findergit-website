cf53ca3da0b64a924edc403f9a6cbe799bad8ea2
Giovambattista Fazioli <gfazioli@users.noreply.github.com>
2026-04-14 18:37:34 +0200

    Initial commit

---
+++ content/_meta.ts
@@ -0,0 +1,21 @@
+export default {
+  index: 'Introduction',
+  '##': {
+    type: 'separator',
+  },
+  markdown: '',
+  'inline-svg': 'Inline SVG',
+  mantine: '',
+  api: '',
+
+  '###': {
+    type: 'separator',
+    // title: 'My Items', // Title is optional
+  },
+  versioning: {
+    theme: {
+      toc: false,
+    },
+  },
+  'release-notes': '',
+};
