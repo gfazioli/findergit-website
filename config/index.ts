6dfad172485860a6230e53f15af2a049b3650d2d
Giovambattista Fazioli <giovambattista.fazioli@gmail.com>
2026-08-06 11:09:01 +0200

    feat(sparkle): publish FinderGit v0.25.0 to appcast

--- config/index.ts
+++ config/index.ts
@@ -92,7 +92,7 @@ export default {
     defaultLanguage: 'en',
   },
   app: {
-    version: '0.24.0',
+    version: '0.25.0',
     minMacOS: '15.0',
     downloadUrl: 'https://github.com/gfazioli/findergit-website/releases/latest',
   },
